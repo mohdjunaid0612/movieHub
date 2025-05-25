9d10cf0d1419f9c3af55db1fe0c057d44c21e2b9
mohdjunaid0612 <mohdjunaid0612@gmail.com>
2025-05-25 23:12:14 +0530

    movie fetch from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,11 @@
-import React, { useState } from "react";
-import { movies } from "./data";
-const App = () => {
-  const [moviesList, setMoviesList] = useState(movies);
+import React from "react";
+// import Movie from "./component/Movie";
+import Home from "./component/Home";
 
-  const movieFilter = (cat) => {
-    setMoviesList(movies.filter((data) => data.category == cat));
-  };
+const App = () => {
   return (
     <>
-      <div className="container">
-        <h3 className="text-warning text-center pt-3">Movies Zone</h3>
-        <div className="text-center my-4 flex flex-wrap">
-          <button
-            onClick={() => setMoviesList(movies)}
-            className="btn btn-primary m-2"
-          >
-            All
-          </button>
-          <button
-            onClick={() => movieFilter("Action")}
-            className="btn btn-secondary m-2"
-          >
-            Action
-          </button>
-          <button
-            onClick={() => movieFilter("Thriller")}
-            className="btn btn-warning m-2"
-          >
-            Thriller
-          </button>
-          <button
-            onClick={() => movieFilter("Animation")}
-            className="btn btn-success m-2"
-          >
-            Animation
-          </button>
-          <button
-            onClick={() => movieFilter("Drama")}
-            className="btn btn-danger m-2"
-          >
-            Drama
-          </button>
-          <button
-            onClick={() => movieFilter("Horror")}
-            className="btn btn-info m-2"
-          >
-            Horror
-          </button>
-          <button
-            onClick={() => movieFilter("Sci-Fi")}
-            className="btn btn-light m-2"
-          >
-            Sci-Fi
-          </button>
-        </div>
-        <div className="row">
-          {moviesList.map((data) => (
-            <div className="col-lg-3 mb-3">
-              <div className="p-2 border itemMovie rounded h-100" key={data.id}>
-                <div className="imgBox rounded mb-3">
-                  <img src={data.backdrop_path} className="w-100 rounded" />
-                </div>
-
-                <h5>{data.title}</h5>
-                <h6 className="text-warning">{data.release_date}</h6>
-                <p className="fw-bold">{data.category}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Home />
     </>
   );
 };
---
+++ src/component/Home.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+
+const api_key = "f10aa479e5ca194f545036149368f781";
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const api = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${page}`
+      );
+      const data = await api.json();
+      setMovies(data.results);
+    };
+
+    fetchMovie();
+  }, [page]);
+  return (
+    <>
+      <div className="container">
+        <h3 className="text-warning text-center my-4 ">Latest Movies</h3>
+        <div className="row">
+          {movies.map((data) => (
+            <div className="col-lg-3 col-lg-3 mb-3">
+              <div className="p-2 border itemMovie rounded h-100" key={data.id}>
+                <div className="imgBox rounded mb-3 ">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200/${data.backdrop_path}`}
+                    className="w-100 rounded"
+                  />
+                </div>
+                <h5>{data.title}</h5>
+                <h6 className="text-warning">{data.release_date}</h6>
+                <p className="fw-bold">{data.category}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="text-center my-3">
+          <button
+            className="btn btn-info mx-2"
+            onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+            disabled={page === 1}
+          >
+            Prev
+          </button>
+          <button
+            className="btn btn-info mx-2"
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
