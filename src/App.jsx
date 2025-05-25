53214f72deb931d3802fcae96d4325e6957f1814
mohdjunaid0612 <mohdjunaid0612@gmail.com>
2025-05-25 19:01:18 +0530

    update movies data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { movies } from "./data";
+const App = () => {
+  const [moviesList, setMoviesList] = useState(movies);
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  const movieFilter = (cat) => {
+    setMoviesList(movies.filter((data) => data.category == cat));
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="container">
+        <h3 className="text-warning text-center pt-3">Movies Zone</h3>
+        <div className="text-center my-4 flex flex-wrap">
+          <button
+            onClick={() => setMoviesList(movies)}
+            className="btn btn-primary mx-2"
+          >
+            All
+          </button>
+          <button
+            onClick={() => movieFilter("Action")}
+            className="btn btn-secondary mx-2"
+          >
+            Action
+          </button>
+          <button
+            onClick={() => movieFilter("Thriller")}
+            className="btn btn-warning mx-2"
+          >
+            Thriller
+          </button>
+          <button
+            onClick={() => movieFilter("Animation")}
+            className="btn btn-success mx-2"
+          >
+            Animation
+          </button>
+          <button
+            onClick={() => movieFilter("Drama")}
+            className="btn btn-danger mx-2"
+          >
+            Drama
+          </button>
+          <button
+            onClick={() => movieFilter("Horror")}
+            className="btn btn-info mx-2"
+          >
+            Horror
+          </button>
+          <button
+            onClick={() => movieFilter("Sci-Fi")}
+            className="btn btn-light mx-2"
+          >
+            Sci-Fi
+          </button>
+        </div>
+        <div className="row">
+          {moviesList.map((data) => (
+            <div className="col-lg-3 mb-3">
+              <div className="p-2 border itemMovie rounded h-100" key={data.id}>
+                <div className="imgBox rounded mb-3">
+                  <img src={data.backdrop_path} className="w-100 rounded" />
+                </div>
 
-export default App
+                <h5>{data.title}</h5>
+                <h6 className="text-warning">{data.release_date}</h6>
+                <p className="fw-bold">{data.category}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
